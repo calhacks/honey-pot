fd6d2f9267c8ffbaeee54f56189d9157da470e25
Corey Mostero <mostero@berkeley.edu>
2025-07-28 04:04:17 -0700

    add: backend fixes + middleware

--- src/schema/rpc.ts
+++ src/schema/rpc.ts
@@ -19,47 +19,56 @@ export class ProfileRpcs extends RpcGroup.make(
 		success: S.Array(Profile.Profile),
 		// TODO: proper failure types
 		error: S.Unknown,
-		payload: {},
+		payload: S.Struct({}),
 	}),
 
 	Rpc.make("GetProfileById", {
 		success: Profile.Profile,
 		// TODO: proper failure types
 		error: S.Unknown,
-		payload: {
+		payload: S.Struct({
 			id: Profile.Profile.fields.id,
-		},
+		}),
 	}),
 
 	Rpc.make("InsertProfile", {
 		success: Profile.Profile,
 		// TODO: proper failure types
 		error: S.Unknown,
-		payload: {
+		payload: S.Struct({
 			user_id: Profile.Profile.fields.user_id,
 			created_at: Profile.Profile.fields.created_at,
 			updated_at: Profile.Profile.fields.updated_at,
 			role: Profile.Profile.fields.role,
 			avatar_url: Profile.Profile.fields.avatar_url,
-		},
+		}),
 	}),
 
 	Rpc.make("UpdateProfile", {
 		success: Profile.Profile,
 		// TODO: proper failure types
 		error: S.Unknown,
-		payload: {
+		payload: S.Struct({
 			role: S.optional(Profile.Profile.fields.role),
 			avatar_url: S.NullishOr(Profile.Profile.fields.avatar_url),
-		},
+		}),
 	}),
 
 	Rpc.make("DeleteProfile", {
 		success: S.Void,
 		// TODO: proper failure types
 		error: S.Unknown,
-		payload: {
+		payload: S.Struct({
 			id: Profile.Profile.fields.id,
-		},
+		}),
 	}),
 ) {}
+
+// export class UserRpcs extends RpcGroup.make(
+// 	Rpc.make("CreateUserWithEmail", {
+// 		success: S.Array(User.User),
+// 		// TODO: proper failure types
+// 		error: S.Unknown,
+// 		payload: S.Struct({}),
+// 	}),
+// ) {}
