20517bd545ceb5cdd0ffec3e1ceb2c780cad6467
Corey Mostero <mostero@berkeley.edu>
2025-11-04 18:06:02 -0800

    add: google oauth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const sfPro = localFont({
 	src: "../assets/fonts/SF_Pro.ttf",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${sfPro.className} antialiased`}>{children}</body>
+			<body className={`${sfPro.className} antialiased`}>
+				{children}
+				<Toaster />
+			</body>
 		</html>
 	);
 }
