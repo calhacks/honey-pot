9a6ef0e645d26a008e990e4e7bc3c3e8a15708e6
Corey Mostero <mostero@berkeley.edu>
2025-11-11 18:33:50 -0800

    add: notice

--- src/lib/supabase/client/browser.ts
+++ src/lib/supabase/client/browser.ts
@@ -3,6 +3,8 @@ import { Console, Effect, Redacted } from "effect";
 import { ClientEnv } from "@/lib/env/client";
 import type { Database } from "@/lib/supabase/database.types";
 
+// Note: The only times you should use this client is to interact with the user object.
+// There should almost never be a reason to interact with the database using this client – use the RPC functions!
 export class SupabaseBrowserClient extends Effect.Service<SupabaseBrowserClient>()(
 	"@honey-pot/src/lib/supabase/client/browser/SupabaseServerClient",
 	{
