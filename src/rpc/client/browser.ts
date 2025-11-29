84d0b7e68fed42a4c22551baaad3a421a9c98f2f
Corey Mostero <mostero@berkeley.edu>
2025-11-29 11:12:23 -0800

    fix: browser client

--- src/rpc/client/browser.ts
+++ src/rpc/client/browser.ts
@@ -3,18 +3,12 @@
 import { FetchHttpClient } from "@effect/platform";
 import { RpcClient, RpcSerialization } from "@effect/rpc";
 import { AtomRpc } from "@effect-atom/atom-react";
-import { Console, Effect, Layer, Redacted } from "effect";
-import { ClientEnv } from "@/lib/env/client";
+import { Layer } from "effect";
 import { Rpcs } from "@/rpc/procedures/definitions";
 
-const Protocol = Layer.unwrapEffect(
-	Effect.gen(function* () {
-		const { NextPublicVercelUrl } = yield* ClientEnv;
-		return RpcClient.layerProtocolHttp({
-			url: `${Redacted.value(NextPublicVercelUrl)}/api/rpc`,
-		});
-	}).pipe(Effect.tapErrorCause(Console.error), Effect.provide(ClientEnv.Default)),
-);
+const Protocol = RpcClient.layerProtocolHttp({
+	url: "/api/rpc",
+});
 
 export class BrowserRpcClient extends AtomRpc.Tag<BrowserRpcClient>()(
 	"@honey-pot/src/rpc/client/browser/BrowserRpcClient",
