57ff0af7620a1125473726b1e249a3703d60455c
Corey Mostero <mostero@berkeley.edu>
2025-11-28 19:31:53 -0800

    wip: logging

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -11,5 +11,8 @@ export class ClientEnv extends Effect.Service<ClientEnv>()("@honey-pot/src/lib/e
 			NextPublicVercelUrl: Config.redacted(NextPublicVercelUrl),
 		});
 		return config;
-	}),
+	}).pipe(
+		Effect.tap(Effect.annotateCurrentSpan),
+		Effect.tapErrorCause((error) => Effect.annotateCurrentSpan({ error: error.toJSON() })),
+	),
 }) {}
